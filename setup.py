eb74fb403ec541e7203cb3fb4013650b7735111f
Anton Sizov <sizeofanton@gmail.com>
2019-12-18 21:59:36 +0300

    PyPi published

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     description='',
     author='Anton Sizov',
     author_email='sizeofanton@gmail.com',
-    url='',
-    download_url='',
+    url='https://github.com/sizeofanton/ucrc',
+    download_url='https://github.com/sizeofanton/ucrc',
     keywords=['CRC','CRC8','CRC16','CRC32','Cyclic redundancy check'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development',
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3'
     ],
 )
